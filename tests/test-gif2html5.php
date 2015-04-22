aeff55d8bcee633964fe2886a7089718e0ef01b9
Mike Patek <mpatek@gmail.com>
2015-04-22 09:28:45 -0400

    Attachment fixtures and testing mime check.

--- tests/test-gif2html5.php
+++ tests/test-gif2html5.php
@@ -2,6 +2,23 @@
 
 class Gif2Html5Test extends WP_UnitTestCase {
 
+	private $gif_id;
+	private $png_id;
+
+	function setUp() {
+		parent::setUp();
+		$this->gif_id = $this->factory->attachment->create_object(
+			'test.gif',
+			0,
+			array( 'post_mime_type' => 'image/gif' )
+			);
+		$this->png_id = $this->factory->attachment->create_object(
+			'test.png',
+			0,
+			array( 'post_mime_type' => 'image/png' )
+			);
+	}
+
 	function testGif2Html5FunctionReturnsCorrectClass() {
 		$this->assertTrue( 'Gif2Html5' === get_class( Gif2Html5() ) );
 	}
@@ -17,5 +34,13 @@ class Gif2Html5Test extends WP_UnitTestCase {
 		$obj2 = Gif2Html5();
 		$this->assertTrue( $obj1 === $obj2 );
 	}
-}
 
+	function testMimeTypeCheckForGifIsTrue() {
+		$this->assertTrue( Gif2Html5()->mime_type_check( $this->gif_id ) );
+	}
+
+	function testMimeTypeCheckForPngIsFalse() {
+		$this->assertFalse( Gif2Html5()->mime_type_check( $this->png_id ) );
+	}
+
+}
