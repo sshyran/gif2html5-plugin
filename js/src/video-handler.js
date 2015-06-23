0936be0bdfaead50e031d0973ffa5416b57d1da0
Nathaniel Taintor <goldenapplesdesign@gmail.com>
2015-06-23 11:27:14 -0700

    Auto play videos when they are visible on the screen
    Removes the play button from gif-videos on mobile. Adds a loading icon
    in its place. A scroll handler detects when the video is visible on the
    screen, and either plays it (on non-iOS browsers) or replaces it with an
    animated gif (for iOS devices).

--- js/src/video-handler.js
+++ js/src/video-handler.js
@@ -1,7 +1,7 @@
 var VideoHandler = (function($) {
 	var Handler = function(videos) {
 		return {
-			handleError : function() {	
+			handleError : function() {
 				videos.each(function(index, video) {
 					var sources = video.querySelectorAll('source'),
 					object = video.querySelector('object'),
@@ -11,26 +11,61 @@ var VideoHandler = (function($) {
 						var gif = $('<img></img>');
 						gif.attr('alt', object.getAttribute('alt'));
 						gif.attr('src', object.getAttribute('data-gif'));
-						gif.attr('srcset', object.getAttribute('srcset'));	
+						gif.attr('srcset', object.getAttribute('srcset'));
 						video.parentNode.replaceChild(gif.get(0), video);
 					});
 
 				});
 			},
 			handleMobile : function() {
-				$('.gif2html5-video-container').on('click', function() {
-					var $container = $(this);
-					if (!$container.hasClass('played')) {
-						$container.addClass('played');
-						var $video = $container.find('video.gif2html5-video');
-						if ($video.length > 0) {
+
+				var $window = $(window),
+					$videos = $('.gif2html5-video-container'),
+					_buffer = null;
+
+				$window.on('scroll', function ( e ) {
+					if ( !_buffer && $videos.length ) {
+						_buffer = setTimeout(function () {
+							checkInView( e );
+							_buffer = null;
+						}, 300);
+					}
+				});
+
+				function checkInView() {
+					var windowBottom = $window.scrollTop() + $window.height();
+
+					$.each($videos, function () {
+						var elemTop = $(this).offset().top;
+
+						if (elemTop < windowBottom) {
+							$videos = $videos.not($(this));
+							startPlaying($(this));
+						}
+					});
+				}
+
+				function startPlaying($videoContainer) {
+					if (!$videoContainer.hasClass('played')) {
+						$videoContainer.addClass('played');
+						var $video = $videoContainer.find('video');
+
+						if (navigator.userAgent.match(/(iPad|iPhone|iPod)/g)) {
+							var $object = $videoContainer.find('object');
+							var gif = $('<img></img>');
+
+							gif.attr('alt',    $object.attr('alt'));
+							gif.attr('src',    $object.data('gif'));
+							gif.attr('srcset', $object.data('srcset'));
+							gif.insertBefore($video);
+							$video.remove();
+						} else {
 							$video.get(0).play();
 						}
 					}
-				});
-			}
+				}
+			},
 		}
 	};
-
 	return Handler;
 })(jQuery);
