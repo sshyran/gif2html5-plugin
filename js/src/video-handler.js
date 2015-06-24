872f4bdcec52eeee5b3a5875bdb15b3374bd4e29
Nathaniel Taintor <goldenapplesdesign@gmail.com>
2015-06-24 07:39:04 -0700

    Abstract fallbackGif to its own function
    The same functionality was being used in two separate places, to handle
    errors and to handle iOS devices. Moved it up into a hidden helper
    function inside VideoHandler, so that it can be called in both places.
    
    Also moved the checkInView and startPlaying function up there as well,
    as it didn't make sense for them to be inside the handleMobile function.

--- js/src/video-handler.js
+++ js/src/video-handler.js
@@ -1,5 +1,51 @@
 var VideoHandler = (function($) {
 	var Handler = function(videos) {
+		var $window = $(window),
+			$videos = $('.gif2html5-video-container');
+
+		function fallbackGif(video) {
+			var $video = $(video),
+				$object = $video.find('object');
+
+			var gif = $('<img></img>')
+				.attr({
+					'class':  $video.attr('class'),
+					'width':  $video.attr('width'),
+					'height': $video.attr('height'),
+					'alt':    $object.attr('alt'),
+					'src':    $object.data('gif'),
+					'srcset': $object.data('srcset'),
+				});
+			gif.insertBefore($video);
+			$video.remove();
+		};
+
+		function checkInView() {
+			var windowBottom = $window.scrollTop() + $window.height();
+
+			$.each($videos, function () {
+				var elemTop = $(this).offset().top;
+
+				if (elemTop < windowBottom) {
+					$videos = $videos.not($(this));
+					startPlaying($(this));
+				}
+			});
+		}
+
+		function startPlaying($videoContainer) {
+			if (!$videoContainer.hasClass('played')) {
+				$videoContainer.addClass('played');
+				var $video = $videoContainer.find('video');
+
+				if (navigator.userAgent.match(/(iPad|iPhone|iPod)/g)) {
+					fallbackGif($video);
+				} else {
+					$video.get(0).play();
+				}
+			}
+		}
+
 		return {
 			handleError : function() {
 				videos.each(function(index, video) {
@@ -8,20 +54,13 @@ var VideoHandler = (function($) {
 					lastsource = sources[sources.length - 1];
 
 					$(lastsource).on('error', function () {
-						var gif = $('<img></img>');
-						gif.attr('alt', object.getAttribute('alt'));
-						gif.attr('src', object.getAttribute('data-gif'));
-						gif.attr('srcset', object.getAttribute('srcset'));
-						video.parentNode.replaceChild(gif.get(0), video);
+						fallbackGif(video);
 					});
-
 				});
 			},
 			handleMobile : function() {
 
-				var $window = $(window),
-					$videos = $('.gif2html5-video-container'),
-					_buffer = null;
+				var _buffer = null;
 
 				$window.on('scroll resize load', function ( e ) {
 					if ( !_buffer && $videos.length ) {
@@ -32,38 +71,6 @@ var VideoHandler = (function($) {
 					}
 				});
 
-				function checkInView() {
-					var windowBottom = $window.scrollTop() + $window.height();
-
-					$.each($videos, function () {
-						var elemTop = $(this).offset().top;
-
-						if (elemTop < windowBottom) {
-							$videos = $videos.not($(this));
-							startPlaying($(this));
-						}
-					});
-				}
-
-				function startPlaying($videoContainer) {
-					if (!$videoContainer.hasClass('played')) {
-						$videoContainer.addClass('played');
-						var $video = $videoContainer.find('video');
-
-						if (navigator.userAgent.match(/(iPad|iPhone|iPod)/g)) {
-							var $object = $videoContainer.find('object');
-							var gif = $('<img></img>');
-
-							gif.attr('alt',    $object.attr('alt'));
-							gif.attr('src',    $object.data('gif'));
-							gif.attr('srcset', $object.data('srcset'));
-							gif.insertBefore($video);
-							$video.remove();
-						} else {
-							$video.get(0).play();
-						}
-					}
-				}
 			},
 		}
 	};
